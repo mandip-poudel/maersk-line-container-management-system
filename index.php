be05350760d2ee468f97f5f1c73b88bfbb5bc870
Mandip Poudel <poudel.mandip@gmail.com>
2019-08-19 16:16:42 +0545

    Push-Code-8-18-019

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Maeriskline | Container Management System</title>
+<meta name="description" content="A Simple Solution to enable Tracking in your Existing Website.">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link href="css/aalpha.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+<!--
+.style1 {color: #CCCCCC}
+-->
+</style>
+</head>
+
+<body>
+<table border="0" cellpadding="0" cellspacing="0" align="center" width="900" style="padding-top:20px;">
+  <tbody><tr>
+    <td width="900"><img src="images/trheader1.jpg" ></td>
+  </tr>
+  <tr>
+    <td><table border="0" cellpadding="0" cellspacing="0" width="900">
+      <tbody><tr>
+        <td bgcolor="#FFCC00">&nbsp;</td>
+      </tr>
+    </tbody></table></td>
+  </tr>
+</tbody></table>
+<table border="0" cellpadding="0" cellspacing="0" align="center" width="900">
+  <tbody><tr>
+    <td bgcolor="#FFFFFF" width="900"><table border="0" align="center" width="99%">
+      <tbody><tr>
+        <td width="22%"><table border="0" align="center" width="185">
+          <tbody><tr bgcolor="#EFF7FC">
+            <td class="aalpha" bgcolor="#0066CC" height="19" width="179"><img src="home_files/navig.gif" height="24" width="113"></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha" height="19"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Features</strong></td>
+          </tr>
+          <tr>
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Demo</strong></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Purchase </strong></td>
+          </tr>
+          <tr>
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Screenshots</strong></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Platform Requirements </strong></td>
+          </tr>
+          <tr>
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> License Agreement </strong></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> FAQ</strong></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha" bgcolor="#FFFFFF"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Testimonials</strong></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha" bgcolor="#FFFFFF"><strong><img src="home_files/arrow_right.gif" height="9" width="9"> Contact</strong></td>
+          </tr>
+          </tbody></table>
+          <br /><img src="images/logo1.png" >
+  <!-- #EndLibraryItem -->
+        </td>
+        <td height="221" width="1%"><img src="home_files/l1.gif" height="292" width="3"></td>
+        <td class="aalpha" width="54%"><p class="navi" align="justify"><strong>Maerisk Container Management System  </strong> is the simplest solution for <strong>Global &amp; Container Tracking</strong>Division. If you need to enable Tracking Option in your existing or new website, this is quickest Software Solution. </p>
+          <p class="navi" align="justify">Maersk Group,a Danish business conglomerate.It is the world's largest container shipping company having customers through 374 offices in 116 countries. It employs approximately 7,000 sea farers and approximately 25,000 land-based people.Maersk Line operates over 600 vessels and has a capacity of 2.6 million TEU. The company was founded in 1928.</p>
+          <p class="navi" align="justify">Operating in 100 countries and transporting goods around the globe, at first glance it would appear Danish shipping company MaerskLine is already handling all the cargo it can manage<strong>.</strong></p>
+          <p class="navi" align="justify"><strong>All The Way</strong></p>
+          <p class="navi" align="justify">"Caution is a virtue. But sometimes an idea comes along. So precious that you can do nothing but follow and give it all you got. See the impact of Maersk going all the way."</p></td>
+        <td class="aalpha" width="2%"><div align="center"><img src="home_files/l2.gif" height="292" width="3"></div></td>
+        <td class="aalpha" width="21%"><table border="0" align="center" width="89%">
+          <tbody><tr bgcolor="#EFF7FC">
+            <td class="aalpha" bgcolor="#008C00" height="19"><img src="home_files/whoca.gif" height="24" width="140"></td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha" height="19"><img src="home_files/arrow_right.gif" height="9" width="9"> Courier Company </td>
+          </tr>
+          <tr>
+            <td class="aalpha"><img src="home_files/arrow_right.gif" height="9" width="9"> Cargo Company </td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha"><img src="home_files/arrow_right.gif" height="9" width="9"> Any Logistics </td>
+          </tr>
+          <tr>
+            <td class="aalpha"><img src="home_files/arrow_right.gif" height="9" width="9"> Shipping Company </td>
+          </tr>
+          <tr bgcolor="#EFF7FC">
+            <td class="aalpha"><img src="home_files/arrow_right.gif" height="9" width="9"> Freight Company </td>
+          </tr>
+        </tbody></table>
+              <br>
+              <table border="0" align="center" width="90%">
+                <tbody><tr bgcolor="#EFF7FC">
+                  <td class="aalpha" bgcolor="#EFF7FC" height="19"><div align="center">
+                      <p><strong> Status Check </strong>:</p>
+                  </div></td>
+                </tr>
+                <tr bgcolor="#EFF7FC">
+                  <td class="aalpha" bgcolor="#FFFFFF" height="19"><div align="center"><a href="track-status.php" target="_self">Click Here </a> </div></td>
+                </tr>
+                <tr bgcolor="#EFF7FC">
+                  <td class="aalpha" bgcolor="#EFF7FC" height="19"><div align="center">
+                      <p><strong> ADMIN Login </strong>:</p>
+                  </div></td>
+                </tr>
+                <tr bgcolor="#EFF7FC">
+                  <td class="aalpha" bgcolor="#FFFFFF" height="19"><div align="center"><a href="login.php" target="_self">Click here </a> </div></td>
+                </tr>
+              </tbody></table>
+              <br></td>
+      </tr>
+    </tbody></table>
+        <table border="0" align="center" width="100%">
+          <tbody><tr bgcolor="#EFF7FC">
+            <td class="aalpha" bgcolor="#FFFFFF" height="19"><div class="lowerstyle style1" align="center"></div></td>
+          </tr>
+      </tbody></table></td>
+  </tr>
+</tbody></table>
+<table border="0" cellpadding="0" cellspacing="0" align="center" width="900">
+  <tbody><tr>
+    <td colspan="2" class="aalpha" bgcolor="#FFCC00" height="25"><div align="center"> Copyright © Mandip Poudel (NP000083). All Rights Reserved.
+	</div></td>
+  </tr>
+  <tr>
+    <td bgcolor="#2284d5" height="40" width="476">&nbsp;</td>
+    <td bgcolor="#2284d5" width="304"><p class="lowerstyle">
+    
+    </p>
+      <p class="lowerstyle">&nbsp;</p></td>
+  </tr>
+</tbody></table>
+
+</body></html>
